4af09e76a348caac939f2404fc4f6020922d21d9
Hiswe <hiswehalya@gmail.com>
2018-10-17 20:52:26 +0700

    EXAMPLE – remove from repo

--- example/plugins/register-global-components.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import Main from '~/components/main'
-import Navigation from '~/components/navigation'
-
-Vue.component(`kn-main`, Main)
-Vue.component(`kn-navigation`, Navigation)
