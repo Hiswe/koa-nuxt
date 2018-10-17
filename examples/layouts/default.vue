d1506a8c4bd05f5dd1616338b77b229cc62e269b
Hiswe <hiswehalya@gmail.com>
2018-10-17 12:09:30 +0700

    EXAMPLE – add a no-script message

--- examples/layouts/default.vue
+++ examples/layouts/default.vue
@@ -12,8 +12,16 @@ export default {
 <template>
   <kn-main>
     <kn-navigation />
+    <noscript class="no-script">
+      Javascript is disabled but no worries everything should be fine
+    </noscript>
     <nuxt />
     <kn-notifications />
   </kn-main>
 </template>
 
+<style scoped>
+.no-script {
+  text-align: center;
+}
+</style>
